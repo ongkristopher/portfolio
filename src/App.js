c0d8d5b8682e5c1858b137b677794d1fcf383779
Kristopher Ong <ongkristopher@gmail.com>
2022-06-04 15:55:15 +0800

    start from scratch and learn Classes on Javascript

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Hello React
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Hello!</h1>
+          <p>My name is Kristopher. I'm a software engineer.</p>
+          <p>I'm always looking forward to working on meaningful projects.</p>
+      </div>
+    )
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    speak() {
+        console.log('I am', this.name, 'and I am', this.age, 'years old')
+    }
+}
+
+const animal1 = new Animal('Simba', 3);
+animal1.speak();
+console.log(animal1);
+
+class Lion extends Animal {
+    constructor(name,age, furColor, speed) {
+        super(name, age);
+        this.furColor = furColor;
+        this.speed = speed;
+    }
+
+    roar() {
+        console.log('ROOOAR! I have', this.furColor, 'fur and I can run', this.speed, 'miles per hour!');
+    }
+}
+
+const lion1 = new Lion('Mufasa', 20, 'golden', 25);
+lion1.speak();
+lion1.roar();
+console.log(lion1);

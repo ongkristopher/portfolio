853cdd1ace56a3b9e78651ba4e55a69f76edae8b
Kristopher Ong <ongkristopher@gmail.com>
2022-06-04 17:28:13 +0800

    added button to learn event handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Hello!</h1>
-          <p>My name is Kristopher. I'm a software engineer.</p>
-          <p>I'm always looking forward to working on meaningful projects.</p>
-      </div>
-    )
-  }
+
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render() {
+        const bio = this.state.displayBio ? (<div>
+                <p>I live in Bulacan, and code every day.</p>
+                <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
+                <p>Beside coding, I also love music and ramen!</p>
+            </div>
+            ) : null;
+
+        return (
+        <div>
+            <h1>Hello!</h1>
+            <p>My name is Kristopher. I'm a software engineer.</p>
+            <p>I'm always looking forward to working on meaningful projects.</p>
+            { bio }
+            <button onClick={this.toggleDisplayBio}>{ !this.state.displayBio ? 'Read More' : 'Show Less'}</button>
+        </div>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -3,35 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-class Animal {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    speak() {
-        console.log('I am', this.name, 'and I am', this.age, 'years old')
-    }
-}
-
-const animal1 = new Animal('Simba', 3);
-animal1.speak();
-console.log(animal1);
-
-class Lion extends Animal {
-    constructor(name,age, furColor, speed) {
-        super(name, age);
-        this.furColor = furColor;
-        this.speed = speed;
-    }
-
-    roar() {
-        console.log('ROOOAR! I have', this.furColor, 'fur and I can run', this.speed, 'miles per hour!');
-    }
-}
-
-const lion1 = new Lion('Mufasa', 20, 'golden', 25);
-lion1.speak();
-lion1.roar();
-console.log(lion1);
